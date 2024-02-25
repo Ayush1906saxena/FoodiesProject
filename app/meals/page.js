c07c737cd4a14e88b2a63755a6fe20dc52fe0ccd
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-02-25 13:48:26 +0530

    Adding meals to the meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,3 +1,24 @@
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+import Link from "next/link";
 export default function MealsPage() {
-  return <h1>This is the meals page</h1>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={classes.highlight}>by you.</span>
+        </h1>
+        <p>
+          Choose your favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share">Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 }
---
+++ components/meals/meal-item.js
@@ -0,0 +1,24 @@
+import classes from "./meal-item.module.css";
+import Image from "next/image";
+import Link from "next/link";
+export default function MealItem({ title, slug, image, summary, creator }) {
+  return (
+    <article className={classes.meal}>
+      <header>
+        <div className={classes.image}>
+          <Image src={image} alt={alt} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h2>{title}</h2>
+          <p>by {creator}</p>
+        </div>
+      </header>
+      <div className={classes.content}>
+        <p className={classes.summary}>{summary}</p>
+        <div className={classes.actions}>
+          <Link href={`/meals/${slug}`}>View Details</Link>
+        </div>
+      </div>
+    </article>
+  );
+}
