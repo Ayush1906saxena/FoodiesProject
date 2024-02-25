34a3ae03bc2ea1e14ce69f756ce7a5c70299bb91
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-02-25 10:36:23 +0530

    Adding highlight css class to nav links

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -1,10 +1,15 @@
+"use client";
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import Image from "next/image";
 import classes from "@/components/main-header/main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
 
 export default function MainHeader() {
+  // Getting the path so that we can decide which nav link to mark as active.
+  const path = usePathname();
+
   return (
     <>
       <MainHeaderBackground />
@@ -17,10 +22,24 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link
+                href="/meals"
+                className={
+                  path.startsWith("/meals") ? classes.active : undefined
+                }
+              >
+                Browse Meals
+              </Link>
             </li>
             <li>
-              <Link href="/meals">Foodies Community</Link>
+              <Link
+                href="/community"
+                className={
+                  path.startsWith("/community") ? classes.active : undefined
+                }
+              >
+                Foodies Community
+              </Link>
             </li>
           </ul>
         </nav>
