22c0378eec3fde4368aa848edb74def848bb6ec4
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-02-25 17:08:09 +0530

    Made the meal detail page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,30 @@
-export default function BlogSlug({ params }) {
+import { getMeal } from "@/lib/meals";
+import classes from "./page.module.css";
+import Image from "next/image";
+export default function MealsDetailPage({ params }) {
+  const meal = getMeal(params.slug);
   return (
     <>
-      <p>This is a dynamic page</p>
-      <p>{params.slug}</p>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: `${meal.instructions}`,
+          }}
+        ></p>
+      </main>
     </>
   );
 }
