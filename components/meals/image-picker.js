37fb2b32b1c701cc86ad7610017d76da65f09166
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-02-26 11:41:07 +0530

    Added preview to image picker component

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,18 +1,45 @@
 "use client";
 import classes from "./image-picker.module.css";
-import { useRef } from "react";
+import Image from "next/image";
+import { useRef, useState } from "react";
 
 export default function ImagePicker({ label, name }) {
+  const [pickedImage, setPickedImage] = useState();
   const imageInput = useRef();
   // A function to handle clicks by the button and forward it to input button
   function handlePickClick() {
     imageInput.current.click();
   }
 
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    if (!file) {
+      setPickedImage(null);
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setPickedImage(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(file);
+  }
+
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!pickedImage && <p>No Image picked yet</p>}
+          {pickedImage && (
+            <Image
+              src={pickedImage}
+              alt="The image selected by the user"
+              fill
+            />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -20,6 +47,8 @@ export default function ImagePicker({ label, name }) {
           accept="image/png, image/jpeg"
           name={name}
           ref={imageInput}
+          onChange={handleImageChange}
+          required
         />
         {/* We want this button to click the input under the hood */}
         <button
