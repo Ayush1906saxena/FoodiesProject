966f91cc9ac9ec696bd102ef58e312a5b17134dd
Ayush Saxena <ayush.saxena2019@vitstudent.ac.in>
2024-02-26 20:07:11 +0530

    Added submission of data to the form

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,21 @@ import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
+  // we use 'use server' to guarantee that the function will run only on the server side
+  async function shareMeal(formData) {
+    "use server";
+    const meal = {
+      title: formData.get("title"),
+      summary: formData.get("summary"),
+      instructions: formData.get("instructions"),
+      image: formData.get("image"),
+      creator: formData.get("name"),
+      creator_email: formData.get("email"),
+    };
+
+    console.log(meal);
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -11,7 +26,8 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        {/* We are use action instead of onSubmit because onSubmit is a client side function */}
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -39,7 +55,7 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
